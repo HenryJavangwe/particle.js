87a6bbf62a3d81981ebe9734399a378ea135d11d
Henry Javangwe <henrysparks1@gmail.com>
2020-05-27 01:55:22 +0200

    created folders tech family investments and property and added related images inside then created a carousel and create four carousel items inside adn styles them using bootstrap by making ech item a bootstrap card.

--- js/owl.js
+++ js/owl.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
         loop:true,
         autoplay:true,
         autoplayTimeout:3000,
-        autoplayHoverPause:false,
+        autoplayHoverPause:true,
         margin:10,
         responsiveClass:true,
         responsive:{
@@ -16,7 +16,7 @@ $(document).ready(function(){
                 items:1,
             },
             600:{
-                items:3,
+                items:2,
             },
             1000:{
                 items:3,
